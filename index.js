c23c87658f2152638d0a71fc4be4daec24551600
ValtteriV <valtteri.vistiaho@gmail.com>
2018-02-12 18:13:58 +0200

    mooore

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const error = (req, res) => {
 
 app.use(error)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
